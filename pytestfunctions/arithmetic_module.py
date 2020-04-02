a8b7f8e72d213296198c1f5f1e14763d11c6e3af
amitgaigawali <amitgaygawali16593@gmail.com>
2020-04-02 19:42:21 +0530

    module file

---
+++ pytestfunctions/arithmetic_module.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    print('addition of two number {} {} '.format(x, y))
+    res = x+y
+    return res
+
+def product(x,y):
+    print('product of two number {} {} '.format(x,y))
+    res = x*y
+
+    return res
+
+def mod(x,y):
+    print('mod of two number {} {} '.format(x, y))
+    res = x%y
+    return res
