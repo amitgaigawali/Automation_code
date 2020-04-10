e911523350a78d09f94cfba3478fddb184b033dc
amitgaigawali <amitgaygawali16593@gmail.com>
2020-04-11 00:10:52 +0530

    new file

---
+++ pytestfunctions/arithmetic_class.py
@@ -0,0 +1,15 @@
+class ArithmeticClass:
+    def add(self, x, y):
+        print('Adding two number {} & {} ' .format(x, y))
+        res = x + y
+        return res
+
+    def mod(self, x, y):
+        print('mod of two number {} & {}' .format(x, y))
+        res = x % y
+        return res
+
+    def product(self, x, y):
+        print('multiplication of two number  {} % {} ' .format(x, y))
+        res = x * y
+        return res
